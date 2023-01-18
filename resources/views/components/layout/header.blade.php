fa3bf0d160d0671b05fa15b795fb4960aa6e8fb5
Moisés Alves Borges <moisesalvesborges@gmail.com>
2023-01-18 19:58:51 -0300

    Montando banco de dados

---
+++ resources/views/components/layout/header.blade.php
@@ -0,0 +1,28 @@
+ <!-- ======= Header ======= -->
+ <header id="header" class="header fixed-top" data-scrollto-offset="0">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="assets/img/logo.png" alt=""> 
+        <!--<h1>HeroBiz<span>.</span></h1> -->
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+
+          <li><a class="nav-link scrollto" href="{{route('home')}}"><span>Home</span></a></li>
+          <li><a class="nav-link scrollto" href="index.html#about">Sobre</a></li>
+          <li><a class="nav-link scrollto" href="index.html#services">Instituições</a></li>
+          <li><a class="nav-link scrollto" href="index.html#portfolio">Artigos / Revistas</a></li>
+          <li><a class="nav-link scrollto" href="index.html#portfolio">Fale Conosco</a></li>
+          
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle d-none"></i>
+      </nav><!-- .navbar -->
+
+      <a class="btn-getstarted scrollto" href="index.html#about">Entrar</a>
+
+    </div>
+  </header><!-- End Header -->
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <x-layout.head :title="$title ?? ''"></x-layout.head>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top" data-scrollto-offset="0">
+    <x-layout.header></x-layout.header>
+  </header><!-- End Header -->
+
+  
+
+  <main id="main">
+    {{$slot}}
+  </main>
+
+  <!-- ======= Footer ======= -->
+  <x-layout.footer></x-layout.footer>
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <x-layout.script></x-layout.script>
+
+</body>
+
+</html>
